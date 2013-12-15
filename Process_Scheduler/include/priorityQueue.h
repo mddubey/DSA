dc18ff5c5bb2a84d67d16f99841d8241fc60baaf
MD Dubey <dubeyg@whatever.com>
2013-12-15 12:17:13 +0530

    Process Scheduler Started

---
+++ Process_Scheduler/include/priorityQueue.h
@@ -0,0 +1,10 @@
+#include "./include/dl_list.h"
+
+typedef struct {
+	void* data;
+	int priority;
+}Queue_element;
+
+DLList* create_queue();
+int enqueue(DLList* queue, Queue_element* element);
+int dequeue(DLList* queue);
