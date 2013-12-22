a8fed21143a73ef3a7130fa6e1caa493c68830dc
MD Dubey <dubeyg@whatever.com>
2013-12-22 13:36:44 +0530

    Iterator added for lists

--- customTypes.h
+++ customTypes.h
@@ -1,1 +1,11 @@
 typedef char String[256];
+
+typedef struct 
+{
+	int accNo;
+	int balance;
+}Account;
+
+int areAccountsEqual(Account expected,Account actual){
+	return expected.accNo == actual.accNo && expected.balance == actual.balance;
+}
