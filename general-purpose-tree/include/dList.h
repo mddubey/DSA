3486b2e408d41684fb67643a9bd5ddf081d30c96
MD Dubey <dubeyg@whatever.com>
2013-12-25 19:17:56 +0530

    new update of link list library updated

--- general-purpose-tree/include/dList.h
+++ general-purpose-tree/include/dList.h
@@ -14,7 +14,7 @@ typedef int (*compare)(void *elementToCompare,void *element);
 DoubleList create();
 int insert(DoubleList* dList,int index,void *element);
 int delete_node(DoubleList* dList,int index);
-void dispose(DoubleList dList);
+void dispose(DoubleList* dList);
 void* getNodeData(DoubleList list, int index);
 void sort(DoubleList dList, compare cmpFunc);
 Iterator getIterator(DoubleList *dList);
