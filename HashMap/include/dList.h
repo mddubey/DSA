eba559570546ecc5b614f28c88d464d889db34cd
MD Dubey <dubeyg@whatever.com>
2013-12-29 13:18:07 +0530

    new Linked List library updated

--- HashMap/include/dList.h
+++ HashMap/include/dList.h
@@ -1,20 +1,16 @@
 #include "../../iterator.h"
-typedef struct type{
-	void *data;
-	struct type *next,*previous;
-} Node;
 
 typedef struct{
 	int length;
-	void *head,*tail;
+	void *head;
 } DoubleList;
 
 typedef int (*compare)(void *elementToCompare,void *element);
 
-DoubleList create();
+DoubleList* create();
 int insert(DoubleList* dList,int index,void *element);
 int delete_node(DoubleList* dList,int index);
-void dispose(DoubleList* dList);
+void dispose(DoubleList *dList);
 void* getNodeData(DoubleList list, int index);
 void sort(DoubleList dList, compare cmpFunc);
 Iterator getIterator(DoubleList *dList);
